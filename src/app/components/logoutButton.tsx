0851894ca8e99eac810a1ea0556f3095d709793b
Ayush-478 <147281294+Ayush-478@users.noreply.github.com>
2025-11-27 17:43:01 +0530

    Update logoutButton.tsx

--- src/app/components/logoutButton.tsx
+++ src/app/components/logoutButton.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import Logout from '../logoutAction'
 import Link from 'next/link'
 
 
 export default function LogoutButton(){
   return (
   <div>
-    <button onClick={Logout} className="bg-red-200 ml-4">Logout</button>
+    <button onClick={} className="bg-red-200 ml-4">Logout</button>
     <Link href="../dashboard/" className="bg-red-200 ml-4">Go to Files</Link>
   </div>
 )}
